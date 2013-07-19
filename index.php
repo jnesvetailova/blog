24ab5f5c923f28f879cf51e9db7054648a71e842
jnesvetailova <jennynesvetailova@yahoo.com>
2013-07-19 16:47:19 -0400

    Update index.php

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
     <head>
         <title>Archives</title>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <link rel="stylesheet" type="text/css" href="index.css">
     </head>
     <body>
         <h1>
