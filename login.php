b52d9822a7dea8e8a75050d60c00cd4e3e9b85a9
hectoronevu <hectoronevu@gmail.com>
2013-07-21 00:25:42 -0400

    Update login.php
    gave its own form, no longer rely on data from index.php

--- login.php
+++ login.php
@@ -1,14 +1,35 @@
 <?php
-error_reporting(0);
+// Connects to your Database 
 include('connect.php');
 
-if (isset($_POST['submit'])) { // if form has been submitted
-    // makes sure they filled it in
-    if (!$_POST['username'] | !$_POST['passwd']) {
-    	die('You did not fill in a required field.');
-    }
+//Checks if there is a login cookie 
 
+if (isset($_COOKIE['ID_my_site'])) {
+
+//if there is, it logs you in and directes you to the members page 
+    $username = $_COOKIE['ID_my_site'];
+    $pass = $_COOKIE['Key_my_site'];
+    $check = mysql_query("SELECT * FROM main WHERE author = '$username'") or die(mysql_error());
+    while ($info = mysql_fetch_array($check)) {
+        if ($pass != $info['password']) {
+            
+        } else {
+            header("Location: members.php");
+        }
+    }
+}
+
+//if the login form is submitted 
+
+if (isset($_POST['submit'])) {
+
+// if form has been submitted
+// makes sure they filled it in
+    if (!$_POST['username'] | !$_POST['pass']) {
+        die('You did not fill in a required field.');
+    }
     // checks it against the database
+
 //    if (!get_magic_quotes_gpc()) {
 //        $_POST['email'] = addslashes($_POST['email']);
 //    }
@@ -17,30 +38,43 @@ if (isset($_POST['submit'])) { // if form has been submitted
     //Gives error if user dosen't exist
     $check2 = mysql_num_rows($check);
     if ($check2 == 0) {
-        die('Username does not exist!');
+        die('That user does not exist in our database. <a href=registration.php>Click Here to Register</a>');
     }
-
     while ($info = mysql_fetch_array($check)) {
-        $_POST['passwd'] = stripslashes($_POST['passwd']);
+        $_POST['pass'] = stripslashes($_POST['pass']);
+        $info['password'] = stripslashes($info['password']);
+        $_POST['pass'] = md5($_POST['pass']);
 
-        $info['passwd'] = stripslashes($info['passwd']);
-
-        $_POST['passwd'] = md5($_POST['passwd']);
         //gives error if the password is wrong
-        if ($_POST['passwd'] != $info['passwd']) {
+        if ($_POST['pass'] != $info['password']) {
             die('Incorrect password, please try again.');
-        } else {
-            // if login is ok then we add a cookie 
+        } else { // if login is ok then we add a cookie      
             $_POST['username'] = stripslashes($_POST['username']);
             $hour = time() + 3600;
             setcookie(ID_my_site, $_POST['username'], $hour);
             setcookie(Key_my_site, $_POST['pass'], $hour);
-            //then redirect them to the members area 
+//then redirect them to the members area 
             header("Location: members.php");
         }
     }
-} else {
-    echo "Please log in!";
+} else {  // if they are not logged in 
 }
-mysql_close($con);
 ?>
+<form action="<?php echo $_SERVER['PHP_SELF'] ?>" method="post"> 
+    <table border="0"> 
+        <tr>
+            <td colspan=2><h1>Login</h1></td>
+        </tr> 
+        <tr>
+            <td>Username:</td>
+            <td> <input type="text" name="username" maxlength="40"> </td>
+        </tr> 
+        <tr>
+            <td>Password:</td>
+            <td> <input type="password" name="pass" maxlength="50"> </td>
+        </tr> 
+        <tr>
+            <td colspan="2" align="right"> <input type="submit" name="submit" value="Login"> </td>
+        </tr> 
+    </table> 
+</form> 
