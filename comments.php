817772011d71a33f0fd04d4a2bfcb819d4bd76ae
bmacc <bdacosta1@student.gsu.edu>
2013-07-21 14:12:04 -0400

    Update comments.php

--- comments.php
+++ comments.php
@@ -5,45 +5,39 @@
         <link rel="stylesheet" type="text/css" href="index.css">
     </head>
     <body>
+    	<div id="body">
+			<div id="comments_blogs">
+				<?php
+					// Connects to your Database 
+					include('connect.php');
+					date_default_timezone_set('America/New_York');
+					//checks cookies to make sure they are logged in 
 
-        <?php
-// Connects to your Database 
-        include('connect.php');
-        date_default_timezone_set('America/New_York');
-//checks cookies to make sure they are logged in 
+					if (isset($_COOKIE['ID_my_site'])) {
+						$username = $_COOKIE['ID_my_site'];
+						$pass = $_COOKIE['Key_my_site'];
+						$check = mysql_query("SELECT * FROM main WHERE author = '$username'") or die(mysql_error());
+						while ($info = mysql_fetch_array($check)) {
+							//if the cookie has the wrong password, they are taken to the login page 
 
-        if (isset($_COOKIE['ID_my_site'])) {
-            $username = $_COOKIE['ID_my_site'];
-            $pass = $_COOKIE['Key_my_site'];
-            $check = mysql_query("SELECT * FROM main WHERE author = '$username'") or die(mysql_error());
-            while ($info = mysql_fetch_array($check)) {
-                //if the cookie has the wrong password, they are taken to the login page 
-
-                if ($pass != $info['password']) {
-                    header("Location: login.php");
-                }
-                //otherwise they are shown the admin area   
-                else {
-                    echo "Welcome " . $username . "<p>";
-                    echo "<a href=index.php>Main page</a> <br />";
-                    echo "<a href=logout.php>Logout</a> <br /> <br />";
-                    ?>
+							if ($pass != $info['password']) {
+								header("Location: login.php");
+								}
+								//otherwise they are shown the admin area   
+							else {								
+								include('commentsII.php');
+					?>
+			</div>	  
+				<div id="post_section">                 
                     <h3>
                         Post
-                    </h3>
-                    <?php
-                    $posts = mysql_query("SELECT * FROM posts WHERE postID = '$_GET[postID]'");
-                    while ($row = mysql_fetch_array($posts)) {
-                        echo $row['title'];
-                        echo " on " . $row['date'];
-                        echo "<br />";
-                        echo $row['content'];
-                        echo "<br /><br />";
-                    }
-                    ?>
+                    </h3>  
+				</div>
+				<div id="comment_section">
                     <h3>
                         Comments
                     </h3>
+                 </div>
                     <?php
                     $result = mysql_query("SELECT * FROM comments WHERE postID = '$_GET[postID]'");
                     while ($row = mysql_fetch_array($result)) {
@@ -76,18 +70,20 @@
             echo $_POST['content'] . "<br /><br />";
         }
         ?>
-        <form action="<?php echo $_SERVER['PHP_SELF'] ?>" method="post"> 
-            <table border="0"> 
-                <tr>
-                    <td>
-                        <textarea rows="4" cols="50" name="content">Care to comment?</textarea>
-                    </td>
-                </tr>
-                <tr>
-                    <td colspan="2" align="left"> <input type="submit" name="submit" value="Post"> </td>
-                </tr> 
-            </table> 
-
-        </form> 
+			<div id="comment_blockII">
+				<form action="<?php echo $_SERVER['PHP_SELF'] ?>" method="post"> 
+					<table border="0"> 
+						<tr>
+							<td>
+								<textarea rows="4" cols="50" name="content">Care to comment?</textarea>
+							</td>
+						</tr>
+						<tr>
+							<td colspan="2" align="left"> <input type="submit" name="submit" value="Post"> </td>
+						</tr> 
+					</table> 
+				</form> 
+			</div>
+		</div>              
     </body>
 </html>
